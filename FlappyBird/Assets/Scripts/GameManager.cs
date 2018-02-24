3be13cf67f4678a5a83b5214b8c2c11ce4f86630
wswiderski <wojtek_swiderski8@gmail.com>
2018-02-24 16:19:00 +0100

    Added game manager script.

---
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameStages
+    {
+        DEMO, PLAY, GAME_OVER
+    }
+
+    private static int points = 0;
+    private static GameStages gameStage;
+
+    private void Awake()
+    {
+        points = 0;
+        gameStage = GameStages.DEMO;
+    }
+
+    private void OnDemoStage()
+    {
+        gameStage = GameStages.DEMO;
+    }
+
+    private void OnPlayStage()
+    {
+        gameStage = GameStages.PLAY;
+    }
+
+    private void OnGameOverStage()
+    {
+        gameStage = GameStages.GAME_OVER;
+    }
+
+    private void ResetGame()
+    {
+        points = 0;
+        gameStage = GameStages.DEMO;
+    }
+
+    public static void AddPoint()
+    {
+        points++;
+    }
+
+    public static GameStages GameStage
+    {
+        get
+        {
+            return gameStage;
+        }
+    }
+
+    public static int Points
+    {
+        get
+        {
+            return points;
+        }
+    }
+}
---
+++ FlappyBird/Assets/Scripts/GameStages.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStages {
+    DEMO, PLAY, GAME_OVER
+}

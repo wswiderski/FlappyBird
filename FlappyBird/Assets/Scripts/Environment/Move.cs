0a0065ec442e77de1c2755ae5b82b8cc183776ca
wswiderski <wojtek_swiderski8@gmail.com>
2018-02-24 11:30:25 +0100

    Added player end environment movement.

---
+++ FlappyBird/Assets/Scripts/Environment/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public static float environmentSpeed = 2.5f;
+
+    public Vector2 respawnPosition = new Vector2(27f, 0f);
+    public float endXPosition = -27f;
+
+    private Vector2 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position.x <= endXPosition)
+        {
+            transform.position = respawnPosition;
+        }
+
+        MoveObject();
+    }
+
+    public void ResetToStartPosition()
+    {
+        transform.position = startPosition;
+    }
+
+    private void MoveObject()
+    {
+        transform.position = new Vector2(
+            transform.position.x - environmentSpeed * Time.deltaTime,
+            transform.position.y);
+    }
+}
---
+++ FlappyBird/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float force = 5f;
+    public float lengthOfDemoFly = 1f;
+    public float demoFlyTimeFactor = 0.7f;
+    public bool canMove = true;
+    public bool moveStart = false;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        DisableGravity();
+    }
+
+    private void FixedUpdate()
+    {
+        if(canMove && !moveStart)
+        {
+            DemoFly();
+        }
+
+        if (canMove)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                EnableGravity();
+                rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    public void DisableGravity()
+    {
+        rb.gravityScale = 0f;
+    }
+
+    public void EnableGravity()
+    {
+        rb.gravityScale = 1f;
+    }
+
+    private void DemoFly()
+    {
+        Vector2 newPosition = new Vector2(0f, Mathf.PingPong(Time.time * demoFlyTimeFactor, lengthOfDemoFly));
+        Debug.Log(newPosition);
+        rb.MovePosition(newPosition);
+    }
+}

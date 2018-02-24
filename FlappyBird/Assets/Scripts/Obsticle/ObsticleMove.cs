bdebb8864f8161bb1f027d7ee4d5a03f8b29022f
wswiderski <wojtek_swiderski8@gmail.com>
2018-02-24 12:19:37 +0100

    Add method to randomlly set obsticle vertical position and distance. Added method to move obsticles.

---
+++ FlappyBird/Assets/Scripts/Obsticle/ObsticleMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleMove : MonoBehaviour {
+
+    private bool canMove;
+	
+	void Update () {
+        transform.position = new Vector2(
+            transform.position.x - EnvMove.environmentSpeed * Time.deltaTime,
+            transform.position.y);
+    }
+}
---
+++ FlappyBird/Assets/Scripts/Obsticle/SetObsticle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObsticle : MonoBehaviour {
+
+    public GameObject lowerColumn;
+    public GameObject upperColumn;
+
+    public Vector2 verticalPositionBorder = new Vector2(-1.5f, 3f);
+    public Vector2 verticalDistance = new Vector2(1.5f, 2.2f);
+
+	void Start () {
+        SetPosition();
+        SetDistance();
+	}
+	
+    private void SetPosition()
+    {
+        transform.position = new Vector2(
+            transform.position.x,
+            Random.Range(verticalPositionBorder.x, verticalPositionBorder.y));
+    }
+
+    private void SetDistance()
+    {
+        float distanceBeetwenObsticles = Random.Range(verticalDistance.x, verticalDistance.y);
+        float distance = distanceBeetwenObsticles / 2f;
+
+        SetOffsetPositionY(lowerColumn, distance);
+        SetOffsetPositionY(upperColumn, -distance);
+    }
+
+    private void SetOffsetPositionY(GameObject obsticle, float distance)
+    {
+        obsticle.transform.position = new Vector2(
+            obsticle.transform.position.x,
+            obsticle.transform.position.y - distance);
+    }
+}

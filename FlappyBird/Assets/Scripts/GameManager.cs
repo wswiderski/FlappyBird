69f5b694ab669267b71eca08c42e7152d23a3d9d
wswiderski <wojtek_swiderski8@gmail.com>
2018-02-25 11:14:08 +0100

    Add info text and change canvas to resolution adjustment and build option.

--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -1,51 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    private const string INFO_TXT = "Tap LMB to start game";
+    private const string GAME_OVER_INFO_TEXT = "Your score: ";
+
     public PlayerMove player;
     public PointUpdater pointUpdater;
+
+    public Text infoText;
 
     private static int points = 0;
     private static GameStages gameStage;
 
     private void Start()
     {
-        points = 0;
-        gameStage = GameStages.DEMO;
+        DemoStage();
+    }
+
+    private void Update()
+    {
+        if(gameStage == GameStages.DEMO && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlayStage();
+        }
     }
 
     private void DemoStage()
     {
+        ResetPoint();
         player.DisableGravity();
+
+        infoText.text = INFO_TXT;
+        infoText.enabled = true;
+
         gameStage = GameStages.DEMO;
     }
 
-    private void PLayStage()
+    private void PlayStage()
     {
+
         player.EnableGravity();
         player.EnableMove();
         player.MoveBirdUp();
+
+        infoText.enabled = false;
+
         gameStage = GameStages.PLAY;
     }
 
     public void GameOverStage()
     {
-        gameStage = GameStages.GAME_OVER;
         player.DisableMove();
-    }
 
-    private void ResetGame()
-    {
-        points = 0;
-        gameStage = GameStages.DEMO;
-        DemoStage();
+        infoText.text = GAME_OVER_INFO_TEXT + points.ToString();
+        infoText.enabled = true;
+
+        gameStage = GameStages.GAME_OVER;
     }
 
     public void AddPoint()
     {
         points++;
+        pointUpdater.UpdatePointsText();
+    }
+
+    public void ResetPoint()
+    {
+        points = 0;
         pointUpdater.UpdatePointsText();
     }
 

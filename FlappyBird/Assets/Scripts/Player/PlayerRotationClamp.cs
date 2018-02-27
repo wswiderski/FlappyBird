cef946d57bec76c8b7638fb1221f8677efe216fa
wswiderski <wojtek_swiderski8@gmail.com>
2018-02-27 23:36:56 +0100

    Add player rotation clamp.

---
+++ FlappyBird/Assets/Scripts/Player/PlayerRotationClamp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotationClamp : MonoBehaviour {
+
+    public Vector2 rotationBoundaries = new Vector2(45f, 280f);
+
+    private void LateUpdate()
+    {
+        float zRotation = transform.localEulerAngles.z;
+        if(zRotation >= rotationBoundaries.x && zRotation <= rotationBoundaries.y)
+        {
+            transform.rotation = zRotation >= 180f ? transform.rotation = Quaternion.Euler(0f, 0f, rotationBoundaries.y) 
+                : transform.rotation = Quaternion.Euler(0f, 0f, rotationBoundaries.x);
+        }
+    }
+}
